6a5bd3c7b99f10466be19b3cd5d5286b73c713d1
Link <linicker11@gmail.com>
2024-09-26 16:36:10 -0300

    Iniciada aula de Matrizes

---
+++ src/Aula19/exercicios/ExercicioNove.java
@@ -0,0 +1,37 @@
+package Aula19.exercicios;
+
+import java.util.Scanner;
+
+public class ExercicioNove {
+    public static void main(String [] args ){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o numero de elementos na array");
+        int num = sc.nextInt();
+
+        int [] A = new int[num];
+        int[] B = new int[A.length];
+        int []C = new int[A.length];
+
+        for(int i =0; i < A.length; i++) {
+            System.out.println("Escreva o " + (i + 1) + " numero de A: ");
+            A[i] = sc.nextInt();
+        }
+
+        for(int i =0; i < B.length; i++) {
+            System.out.println("Escreva o " + (i + 1) + " numero de B: ");
+            B[i] = sc.nextInt();
+        }
+        for(int i = 0; i < A.length; i++){
+            System.out.print("A = " + A[i] + " . ");
+        }
+        System.out.println();
+        for(int i = 0; i < B.length; i++){
+            System.out.print("B = " + B[i] + " . ");
+        }
+        System.out.println();
+        for(int i = 0; i < A.length; i++){
+            C[i] = A[i] / B[i];
+            System.out.print("C = " + C[i] + " . ");
+        }
+    }
+}
---
+++ src/Aula20/Matrizes.java
@@ -0,0 +1,34 @@
+package Aula20;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Matrizes {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+
+        double [][] notaAlunos  = new double[3][4];//declarando matriz
+        notaAlunos [0][0] = 10.0;
+        notaAlunos [0][1] = 5.0;
+        notaAlunos [0][2] = 9.0;
+        notaAlunos [0][3] = 10.0;
+
+        notaAlunos [1][0] = 9;
+        notaAlunos [1][1] = 11;
+        notaAlunos [1][2] = 15;
+        notaAlunos [1][3] = 14;
+
+        notaAlunos [2][0] = 11;
+        notaAlunos [2][1] = 19;
+        notaAlunos [2][2] = 12;
+        notaAlunos [2][3] = 13;
+
+        for(int i = 0; i < notaAlunos.length; i++){
+            for( int j = 0; j < notaAlunos[i].length; j++){
+                System.out.print(notaAlunos[i][j] + "...");
+            }
+            System.out.println();
+        }
+    }
+}
