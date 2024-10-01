237465bb98d15f753be656f029f28f5295750b34
Link <linicker11@gmail.com>
2024-10-01 17:51:04 -0300

    Aula Controladores

---
+++ src/Aula29/Carro.java
@@ -0,0 +1,10 @@
+package Aula29;
+
+public class Carro {
+
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consumoCombustivel;
+}
---
+++ src/Aula29/TesteCarro.java
@@ -0,0 +1,22 @@
+package Aula29;
+
+import Aula29.Carro;
+
+import java.util.Scanner;
+
+public class TesteCarro {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        String s = scan.nextLine();
+        int a = scan.nextInt();
+        // Write your code here.
+        System.out.println("Int: " + i);
+        System.out.println("Double: " + d);
+        System.out.println("String: " + s);
+    }
+}
