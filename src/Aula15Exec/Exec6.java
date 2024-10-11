44e63446effe925af018162a6eda96968ffa0fb4
Link <linicker11@gmail.com>
2024-10-11 17:17:27 -0300

    Exercicio - 1un

---
+++ src/Aula15Exec/Exec6.java
@@ -0,0 +1,23 @@
+package Aula15Exec;
+
+import java.util.Scanner;
+
+public class Exec6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite primeiro numero: ");
+        int num1 = sc.nextInt();
+        System.out.println("Digite segundo numero: ");
+        int num2 = sc.nextInt();
+        System.out.println("Digite terceiro numero: ");
+        int num3 = sc.nextInt();
+
+        if (num1 > num2 && num1 > num3){
+            System.out.println("O primeiro número: "+num1 + ":  é maior ");
+        } else if(num2 > num3){
+            System.out.println("O segundo número: "+num2 + ":  é maior ");
+        }else{
+            System.out.println("O terceiro número: "+num3 + ":  é o maior");
+        }
+    }
+}
