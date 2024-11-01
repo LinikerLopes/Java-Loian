67529967beaa0bae4eb418f096ed0998b1a0ce53
LinikerLopes <56650677+LinikerLopes@users.noreply.github.com>
2024-11-01 09:39:14 -0300

    Update Exercicio4.java

--- src/Aula20Exec/Exercicios/Exercicio4.java
+++ src/Aula20Exec/Exercicios/Exercicio4.java
@@ -1,9 +1,34 @@
 package Aula20Exec.Exercicios;
 
 
+import java.util.Scanner;
 
 public class Exercicio4 {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
 
+
+        String[][] compromissos = new String[31][24];
+
+        boolean sair = false;
+        byte opcao;
+        while (!sair){
+            System.out.println("Digite 1");
+            System.out.println("Digite 2");
+            System.out.println("Digite 3");
+
+            opcao = sc.nextByte();
+
+            if(opcao == 1){
+                System.out.println("Entre com o dia do mês");
+                int dia = sc.nextInt();
+            }else if(opcao == 2){
+
+            }else if(opcao == 0){
+                sair = true;
+            } else{
+                System.out.println("Opcao invalida, diigite novamente");
+            }
+        }
     }
 }
