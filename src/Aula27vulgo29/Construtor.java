ff87dfccc0270a21076dc6d92c5050abd489afdb
Link <linicker11@gmail.com>
2024-11-10 16:11:23 -0300

    This.

--- src/Aula27vulgo29/Construtor.java
+++ src/Aula27vulgo29/Construtor.java
@@ -1,4 +1,11 @@
 package Aula27vulgo29;
 
 public class Construtor {
+String marca;
+String modelo;
+
+    Construtor(String marca_){
+        marca =  marca_;
+        System.out.println("Classe 1");
+    }
 }
---
+++ src/Aula27vulgo29/MainConstrutor.java
@@ -0,0 +1,11 @@
+package Aula27vulgo29;
+
+public class MainConstrutor {
+    public static void main(String[] args) {
+
+        Construtor construtor = new Construtor("Flor");
+
+        System.out.println(construtor.marca);
+
+    }
+}
