3d1200bd8b8c596347b6eea133f160fa3bc03875
Link <linicker11@gmail.com>
2024-11-12 17:53:58 -0300

    okokOk

--- src/Aula27vulgo29/Construtor.java
+++ src/Aula27vulgo29/Construtor.java
@@ -3,9 +3,20 @@ package Aula27vulgo29;
 public class Construtor {
 String marca;
 String modelo;
-
-    Construtor(String marca_){
-        marca =  marca_;
+String ano;
+   /* Construtor(String marca_){
         System.out.println("Classe 1");
+    }*/
+
+    public Construtor(String marca, String modelo, String ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        System.out.println("Construtor com 3");
+    }
+
+    public Construtor(String marca, String modelo){
+        this(marca, modelo, "2010");
+        System.out.println("Construtor com 2");
     }
 }
--- src/Aula27vulgo29/MainConstrutor.java
+++ src/Aula27vulgo29/MainConstrutor.java
@@ -3,9 +3,10 @@ package Aula27vulgo29;
 public class MainConstrutor {
     public static void main(String[] args) {
 
-        Construtor construtor = new Construtor("Flor");
+        Construtor construtor = new Construtor("flor", "Fiat");
 
-        System.out.println(construtor.marca);
+        System.out.println(construtor.ano);
+
 
     }
 }
