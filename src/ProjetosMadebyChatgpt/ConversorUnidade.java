f949e52082bc7ccfefe0c9acebd98b276aa4f9db
Link <linicker11@gmail.com>
2025-02-01 16:41:02 -0300

    mais um projetobasico

---
+++ src/ProjetosMadebyChatgpt/ConversorUnidade.java
@@ -0,0 +1,21 @@
+package ProjetosMadebyChatgpt;
+
+import java.util.Scanner;
+
+public class ConversorUnidade {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        boolean estado = false;
+
+        while(!estado){
+
+            System.out.println("Conversor");
+
+        }
+
+
+    }
+
+}
